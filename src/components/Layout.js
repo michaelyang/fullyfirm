6dd4035d71448a25e6812c5b8b12fa5cd944a640
Michael Yang <myang@Michael-Yang.cable.rcn.com>
2018-12-19 09:45:18 -0500

    Restructuring Navbar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,7 +14,7 @@ const LayoutWrapper = styled.div`
   flex-direction: column;
 `;
 const ContentWrapper = styled.div`
-  padding: 0 0 0 5%;
+  padding: 0 10% 0 10%;
   min-height: fill-available;
 `;
 
